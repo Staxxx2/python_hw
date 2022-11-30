0813170268999aea1f339349f49de230d3001785
Staxxx <sersersin@gmail.com>
2022-12-01 00:21:25 +0300

    hw_1_2 done

--- hw_1/hw_1_2.py
+++ hw_1/hw_1_2.py
@@ -3,3 +3,14 @@
 # ¬ - Отрицание ⋁ - логическое "Или" ⋀ - логическое "И"
 
 
+x = [True, False]
+y = [True, False]
+z = [True, False]
+
+for a in x:
+    for b in y:
+        for c in z:
+            print(a, b, c)
+            r1 = not (a or b or c)
+            r2 = (not a) and (not b) and (not c) 
+            print(r1 == r2)
